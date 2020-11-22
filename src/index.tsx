c9018b779dd8d7e9fda5442a5aba55d23aa4d8b1
Tunahan Karlibas <dvt.tnhn.krlbs@gmail.com>
2020-11-22 04:31:26 +0300

    Format entry file and the web-vitals file

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import "@blueprintjs/core/lib/css/blueprint.css";
+import '@blueprintjs/core/lib/css/blueprint.css';
 // import "@blueprintjs/icons/lib/css/blueprint-icons.css";
 
 ReactDOM.render(
