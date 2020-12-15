abcd1bb4be86c556d72f05d7c06ddb291d1b7562
Burak Çiçek <burakcicek323@gmail.com>
2020-12-15 03:10:05 +0300

    Add Topbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,50 @@
-import React from 'react';
 import { Router } from '@reach/router';
-import { WorldwideCases } from './pages/WorldwideCases';
+import { Menu, Sticky } from 'semantic-ui-react';
+import React, { useState, useRef } from 'react';
 import { LoginForm } from './pages/LoginForm';
+import { WorldwideCases } from './pages/WorldwideCases';
 import { CountryCases } from './pages/CountryCases';
 
 const App: React.FC = () => {
+  const contextRef = useRef(null);
+  const [activeItem, setActiveItem] = useState('gamepad');
+
+  const handleItemClick = (name: string) => setActiveItem(name);
   return (
-    <Router>
-      <WorldwideCases path="/" />
-      <LoginForm path="login" />
-      <CountryCases path=":country" />
-    </Router>
+    <div ref={contextRef}>
+      <Sticky context={contextRef}>
+        <Menu size="large">
+          <Menu.Item
+            name="gamepad"
+            active={activeItem === 'gamepad'}
+            onClick={(_e, { name }) => handleItemClick(name!)}
+          >
+            Games
+          </Menu.Item>
+
+          <Menu.Item
+            name="video camera"
+            active={activeItem === 'video camera'}
+            onClick={(_e, { name }) => handleItemClick(name!)}
+          >
+            Channels
+          </Menu.Item>
+
+          <Menu.Item
+            name="video play"
+            active={activeItem === 'video play'}
+            onClick={(_e, { name }) => handleItemClick(name!)}
+          >
+            Videos
+          </Menu.Item>
+        </Menu>
+      </Sticky>
+      <Router>
+        <WorldwideCases path="/" />
+        <LoginForm path="login" />
+        <CountryCases path=":country" />
+      </Router>
+    </div>
   );
 };
 
